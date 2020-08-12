2aca57a943ff9accc1e570977512ee4a5d628611
Jean-Baptiste Castillo <castillo.jbaptiste@gmail.com>
2020-08-12 12:02:35 +0200

    iteracion 1 & 2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import foods from './foods.json';
 import './App.css';
+import FoodBox from './components/foodbox/FoodBox';
+import 'bulma/css/bulma.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    foods: foods,
+  };
+
+  render() {
+    return (
+      <div className="pt-2 pl-2">
+        {this.state.foods.map((food, index) => {
+          return <FoodBox food={food} />;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
